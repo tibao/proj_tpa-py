a7f5f7acb001e43c6ac1dc576d95fc7f21b42e9b
Vinicius Tibo <viniciustiboff@gmail.com>
2024-11-19 16:55:37 -0300

    commit

--- aula12/Main.py
+++ aula12/Main.py
@@ -1,9 +1,8 @@
-import Pessoa   
+import Pessoa
 import os
 def limpar(): os.system('clear')
 
 limpar()
-
 def pergunta():
     res = int(input("Deseja cadastrar uma nova pessoa? 1 - SIM ou 0 - NÃO: "))
     return res
@@ -23,6 +22,21 @@ while(res == 1):
 def mostrar():
     print("{<4}{<10}{<7}{<10}{<7}"
           .format("N°","Nome","Idade","Cargo","Salário"))
+    y = 1
     for x in cadastro:
         print("{<4}{<10}{<7}{<10}{<7}".
-              format())
+              format(y,
+                     x.get_nome(),
+                     x.get_idade(),
+                     x.get_cargo(),
+                     x.get_salario(),
+                     ))
+        y =+1
+def alterar(x,y,z):
+    if(y==0):cadastro[x].set_nome(z)
+    if(y==1):cadastro[x].set_idade(z)
+    if(y==2):cadastro[x].set_cargo(z)
+    if(y==3):cadastro[x].set_salario(z)
+
+alterar(0,0, "Caique")
+mostrar()
